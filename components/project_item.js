2e3ce85afcc73b35e342567c7d38f69429401dc7
Yong Liang <yoliang@ucsd.edu>
2023-03-20 01:05:23 -0700

    change card implementation

--- components/project_item.js
+++ components/project_item.js
@@ -4,35 +4,30 @@ import Link from "next/link";
 
 const ProjectItem = ({ children, imgSrc, delay, link, stack, order }) => {
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-        y: 50,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-      }}
-      transition={{
-        duration: 0.5,
-        ease: "easeInOut",
-        delay: delay,
-      }}
-      exit={{ y: 25, opacity: 0 }}
-      className="card bg-base-100 shadow-xl hover:shadow-2xl"
-    >
-      <Link href={link}>
+    <Link href={link}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+          delay: delay,
+        }}
+        exit={{ y: 25, opacity: 0 }}
+        className="card bg-base-100 shadow-xl hover:shadow-2xl"
+      >
         <figure className="relative w-full aspect-video">
-          <Image
-            src={imgSrc}
-            alt="image"
-            fill
-            className="mx-auto rounded-2xl"
-          />
+          <Image src={imgSrc} alt="image" fill className="mx-auto" />
         </figure>
         <div className="card-body">{children}</div>
-      </Link>
-    </motion.div>
+      </motion.div>
+    </Link>
   );
 };
 
